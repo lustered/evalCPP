b7350f59f35a317b19c2ff1e10f42b9c13ace710
lustered <smellslikelavenders@gmail.com>
2020-02-20 17:05:21 -0500

    from argv

--- parser.cpp
+++ parser.cpp
@@ -8,14 +8,13 @@ using namespace std;
 vector<string> protos(string statement);
 vector<int> parser(vector<string> &conds);
 
-/* globals */
-/* vector<int> gates; */
-int main(){
+int main(int argc, char** argv){
     // this will be the logic statement
     string l_statement = ("not ( p and q ) and ( q and p )");
     string l_statement2 = ("not ( not p and not q )");
-    /* dividing logic_statement into separate words */
-    vector<string> conds = protos(l_statement2);
+    string from_arg = argv[1]; 
+    /* splitting logic statement into separate characters */
+    vector<string> conds = protos(from_arg);
     vector<int> test = parser(conds);
 
     for(int i = 0; i < test.size();i++){
@@ -32,9 +31,8 @@ vector<string> protos(string statement){
 
     while(ss >> buffer)
         words.push_back(buffer);
-       
-    int words_size = static_cast<int>(words.size());
 
+    /* int words_size = static_cast<int>(words.size()); */
    return words; 
 }
 
@@ -48,21 +46,26 @@ vector<int> parser(vector<string> &conds){
         /* first we will check for NOTs before paren */
         if(conds[i] == "not" && conds[i+1] == "("){
             not_before_paren = true;
-            cout << "found a not" << endl;
+            cout << "found a distributive not" << endl;
         }
         /* first get conditional statements with their values eg:1/0 */
         if(conds[i] == "p" || conds[i] == "q"){
             ret.push_back(1);
             cout << "found : " << conds[i] << endl;
             /* TODO:
-             * working properly, need more test cases and */ 
-            /*     implement gates and nested conditions */
+               implement gates and nested conditions */
+
+            /* note: */ 
+            /*     since "not(not p)" returns 1 and 1 is the default */ 
+            /*     value we input if we find p or q, then we don't */ 
+            /*     change anything in that case */
+
 
            /* if p or q have a not in front then flip their value */
             if(conds[i-1] == "not" && not_before_paren == false){
                 ret.at(ret.size() - 1) = 1 - ret.back();
             }
-            /* if there is no distributed negation */
+            /* if there is distributive negation */
             /*     eg. not(p and q) */
             if(conds[i-1] != "not" && not_before_paren){
                 ret.at(ret.size() - 1) = 1 - ret.back();
