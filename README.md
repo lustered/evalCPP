7fc1b3712fa5cc9cba352abb4b54145b2368cb9a
lustered <smellslikelavenders@gmail.com>
2020-07-21 08:14:08 -0400

    cleanup

---
+++ README.md
@@ -0,0 +1,26 @@
+# logiccino
+# logic gates project using the app Blynk
+- As of now it has 5 gates:
+    *and, or, not, nand, nor 
+- Virtual pins on the app's widgets:
+    
+    input A = vpin 1
+
+    input B = vpin 2
+
+    gate menu = vpin 3
+
+    3x3 table = vpin 4
+
+    terminal for parsing = vpin 5
+
+    output = vpin 6
+
+# terminal will take a conditional statement in form of string 
+# and parse it into truth tables
+
+    eg. ("not ( p and not q )") 
+
+    app displays the corresponding truth table
+
+![reference logic gates table](https://github.com/lustered/logic_gates_blynk/blob/master/gates.jpeg)
---
+++ logiccino/src/main.cpp
@@ -0,0 +1,145 @@
+using namespace std;
+#include <Arduino.h>
+#include <iostream>
+#include <Blynk.h>
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+//libraries needed for conditional statement parser
+//prototypes for parser
+int logic_gate(int gate,int a,int b);
+
+// hotspot credential setup
+char auth[] = "BBKE4i5rjIHcYOZkEaU6zQYUacGz2fXq";
+char ssid[] = "antei";
+char pass[] = "password";
+// wdiget and libs init
+WidgetTerminal terminal(V5);
+BlynkTimer timer;
+
+//global variables for readings
+int gate_selected;
+int A_val;
+int B_val;
+
+//----------------------------------------------------------------------
+//get data from buttons 
+// A 
+BLYNK_WRITE(V1){
+    A_val= param.asInt();
+    Serial.print(A_val);
+}
+// B
+BLYNK_WRITE(V2){
+    B_val = param.asInt();
+    Serial.print(B_val);
+}
+//----------------------------------------------------------------------
+//send the output
+void output_result(){
+int result = logic_gate(gate_selected,A_val,B_val);
+Blynk.virtualWrite(V6,result);
+}
+//----------------------------------------------------------------------
+// gates menu || only need to get current value of the menu
+BLYNK_WRITE(V3) {
+gate_selected = param.asInt();
+}
+//----------------------------------------------------------------------
+// table  
+void sendEvent() {
+// [gate in order lister][index in order 0-3]
+vector<vector<int>> gates{{ 0,0,0,1},// and
+                          { 0,1,1,1},// or
+                          { 1,0,1,0},// not
+                          { 1,1,1,0},// nand
+                          { 1,0,0,0},//nor
+                          { 0,1,1,0}};//xor
+
+if(gate_selected > 0){
+    int gate_index = gate_selected - 1;
+    if(gate_index != 2){
+    Blynk.virtualWrite(V4, "add", 1, "0         0", gates[gate_index][0]);
+    Blynk.virtualWrite(V4, "add", 2, "0         1", gates[gate_index][1]);
+    Blynk.virtualWrite(V4, "add", 3, "1         0", gates[gate_index][2]);
+    Blynk.virtualWrite(V4, "add", 4, "1         1", gates[gate_index][3]);
+}
+else if(gate_index == 2){
+   Blynk.virtualWrite(V4,"clr");
+   Blynk.virtualWrite(V4, "add", 1, "1          ",0);
+   Blynk.virtualWrite(V4, "add", 2, "0          ",1); 
+}
+else
+    delay(1000);
+ }
+}
+
+void setup() {
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+
+  // terminal stuff
+  // put your setup code here, to run once:
+ // -------------------------------------------------
+ // table clear on start
+ Blynk.virtualWrite(V4,"clr");
+ timer.setInterval(1000L, sendEvent);
+ // -------------------------------------------------
+ // give result to output widget
+ timer.setInterval(10L, output_result);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Blynk.run();
+  timer.run();
+}
+//***********************************************************************************************
+// logic gate calculation
+int logic_gate(int gate,int a,int b){
+    int ret = 0;
+    switch(gate){
+        /* and */
+        case 1:
+            if (a == b){
+                ret = a;
+            }
+            break;
+        /* or */
+        case 2:
+            if (a == 1 || b == 1){
+                ret = 1;
+            }
+            break;
+        /* not */
+        case 3:
+            if (a == 1){
+                ret = 0;
+            }
+            else if (a == 0)
+                    ret = 1;
+
+            break;
+        /* nand */
+        case 4:
+            if (a + b < 2){
+                ret = 1;
+            }
+            break;
+        /* nor */
+        case 5:
+            if (a == 0 && b == 0){
+                ret = 1;
+            }
+            break;
+        case 6:
+            if (a != b){
+                ret = 1;
+            }
+            break;
+        default:
+            cout << "error404" << endl;
+
+        }
+    return ret;
+}
